9f4685c84d5c5c916b978f4ec5484104a19debfd
Amine Hmida <aminehmida@gmail.com>
2019-12-26 22:21:55 +0000

    Add support for prefix and directly typing codes

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ PREFIX=${PREFIX:-~/.local}
 echo $PREFIX
 mkdir -p $PREFIX/share/kservices5
 mkdir -p ~/.config/autostart-scripts
+mkdir -p ~/.config/yubioath-krunner
 cp yubioath-krunner.desktop $PREFIX/share/kservices5
 cp yubioath-krunner.py ~/.config/autostart-scripts
+cp ./settings.cfg ~/.config/yubioath-krunner
